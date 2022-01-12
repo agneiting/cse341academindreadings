f170fb9ef1135e2f14d664edc06b2a0e14785a3d
agneiting <gneitinga@byui.edu>
2022-01-12 16:30:07 -0700

    week 1 readings done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const routes = require('./routes');
 
-console.log(routes.someText);
+//console.log(routes.someText);
 
 const server = http.createServer(routes.handler);
 
