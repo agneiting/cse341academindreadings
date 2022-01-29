2574d3edbe1e0391f2dc197960e1c883a14fbf40
agneiting <gneitinga@byui.edu>
2022-01-28 21:11:37 -0700

    first commit

--- util/database.js
+++ util/database.js
@@ -1,4 +1,29 @@
-const mongodb = require('mongodb)');
+const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-MongoClient.connect('mongodb+srv://dbadmin:3nDWCrhfMPuJFb2r@cit341.fk1cs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then().catch();
+let _db;
+
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://dbadmin:3nDWCrhfMPuJFb2r@cit341.fk1cs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
