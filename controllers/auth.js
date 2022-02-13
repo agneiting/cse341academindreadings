916461550f6a5fc061de480cc6d106b5c0e0a032
agneiting <gneitinga@byui.edu>
2022-02-12 19:13:25 -0700

    done i think

--- controllers/auth.js
+++ controllers/auth.js
@@ -112,7 +112,11 @@ exports.postLogin = (req, res, next) => {
           res.redirect('/login');
         });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 
@@ -161,7 +165,9 @@ exports.postSignup = (req, res, next) => {
       console.log(err);
     })
     .catch(err => {
-      console.log(err);
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -218,7 +224,9 @@ exports.postReset = (req, res, next) => {
         // });
       })
       .catch(err => {
-        console.log(err);
+        const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
       });
   });
 };
@@ -247,7 +255,9 @@ exports.getNewPassword = (req, res, next) => {
       });
     })
     .catch(err => {
-      console.log(err)
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -278,6 +288,8 @@ exports.postNewPassword = (req, res, next) => {
       res.redirect('login');
     })
     .catch(err => {
-      console.log(err);
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
